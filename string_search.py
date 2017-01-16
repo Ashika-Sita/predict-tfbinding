62a8d4c2052f812e9b56b794810053840e5fed20
Ashika-Sita <jayanthyashika@gmail.com>
2017-01-15 21:28:38 -0600

    Updated to only give list of ids

--- string_search.py
+++ string_search.py
@@ -30,19 +30,6 @@ def search_fasta(string_file, input_iterator):
 
     for item in input_iterator:
 
-        location_list = [(position, sequence) for position, sequence in motif_object.instances.search((item.seq).upper())]
-        chrom_location = item.description.split(" ")[1]
-        motif_dictionary[item.id+"--"+chrom_location] = location_list
         motif_list.append(item.id)
 
     return motif_list
-
-def write_outfile(motif_dictionary, outfile):
-
-    outfile = open(outfile, 'w')
-
-    for item in motif_dictionary.keys():
-        outfile.write(str(item.split("--")[0]) + "\t" + str(item.split("--")[1]) + "\t" + str(motif_dictionary[item]) + "\t" + str(len(motif_dictionary[item])) + "\n")
-    outfile.close()
-
-    return
